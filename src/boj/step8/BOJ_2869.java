66b5f86d04ab7c1976ad18c7ecc306117b276723
ddangme <ssilver121@naver.com>
2024-06-11 19:33:37 +0900

    feat: add boj step8

---
+++ src/boj/step8/BOJ_2869.java
@@ -0,0 +1,21 @@
+package boj.step8;
+
+import java.util.Scanner;
+
+public class BOJ_2869 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int plus = sc.nextInt();
+        int minus = sc.nextInt();
+        int height = sc.nextInt();
+
+        int day = (height - minus) / (plus - minus);
+
+        if ((height - minus) % (plus - minus) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+    }
+}
---
+++ src/boj/step8/BOJ_2903.java
@@ -0,0 +1,17 @@
+package boj.step8;
+
+import java.util.Scanner;
+
+public class BOJ_2903 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int pow = (int) Math.pow(2, n - 1);
+
+        int sum = pow + pow + 1;
+
+        System.out.println(sum * sum);
+    }
+}
