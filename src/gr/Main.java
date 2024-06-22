4e73fffa3bae1d757ad14fb2b8ea361cb6eca6df
ddangme <ssilver121@naver.com>
2024-06-22 20:10:40 +0900

    feat: 테스트 코드 추가

---
+++ src/gr/Main.java
@@ -0,0 +1,18 @@
+package gr;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer65
+        int[] array = {5,4,3,2,1};
+        Arrays.sort(array);
+        for (int i : array) {
+            System.out.println("i = " + i);
+        }
+    }
+
+}
