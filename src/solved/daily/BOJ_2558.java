eae1e549a1d6d1a95d1a7adf8a2d7e2c91c7c4b3
ddangme <ssilver121@naver.com>
2024-06-23 20:49:18 +0900

    feat: 백준 2558 문제 풀이

---
+++ src/solved/daily/BOJ_2558.java
@@ -0,0 +1,16 @@
+package solved.daily;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2558 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a + b);
+    }
+}
