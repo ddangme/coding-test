36356c2896bec07e0f3aa5f8a05b61f9c399e358
ddangme <ssilver121@naver.com>
2024-06-25 21:21:13 +0900

    feat: 백준 24263 문제 풀이

---
+++ src/boj/step11/BOJ_24263.java
@@ -0,0 +1,16 @@
+package boj.step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
